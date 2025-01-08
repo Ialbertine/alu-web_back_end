71b9af7f43edb9572328c198ba7e697fc5b653b0
Ialbertine <a.ingabire1@alustudent.com>
2025-01-08 18:48:13 +0200

    variable annotations tasks

---
+++ 9-element_length.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python3
+""" this is a function with annotated parameters.
+and return values with appropriate types."""
+
+from typing import List, Union, Tuple, Sequence, Iterable
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+  return [(i, len(i)) for i in lst]
