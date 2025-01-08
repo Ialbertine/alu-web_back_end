e8b4bca4f1839abdadb2103fbdf97c26fa521142
Ialbertine <a.ingabire1@alustudent.com>
2025-01-08 18:51:31 +0200

    variable annotations

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# python question one addition of two float numbers
+
+def add(a: float, b: float) -> float:
+  return a + b
---
+++ python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+### This is Annotations variables tasks
